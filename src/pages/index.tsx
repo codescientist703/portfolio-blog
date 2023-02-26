f23adab48ca930459324e77b6d181b1ebfd4f0bb
Nihal Mittal <nihalmittal47@gmail.com>
2023-02-26 20:49:04 +0530

    fix: added seo component for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { AboutSection } from "components/AboutSection";
 import { ContactSection } from "components/ContactSection";
 import { HomeSection } from "components/HomeSection";
+import { SEO } from "components/SEO";
 import { DefaultLayout } from "layouts/Default";
 
 export default function Home() {
 	return (
 		<DefaultLayout>
+			<SEO />
 			<HomeSection />
 			<AboutSection />
 			<ContactSection />
