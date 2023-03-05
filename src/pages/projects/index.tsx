da990c892ae0df77c7340d978dd11278a8d833d0
Nihal Mittal <nihalmittal47@gmail.com>
2023-03-05 17:47:04 +0530

    fix: added SEO component for projects page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "components/Container";
 import { PageTitle } from "components/PageTitle";
 import { ProjectCard } from "components/ProjectCard";
+import { SEO } from "components/SEO";
 import { DefaultLayout } from "layouts/Default";
 import { InferGetStaticPropsType } from "next";
 import { Project } from "types/project";
 import allProjects from "../../../content/pages/projects.json";
+import personalInfo from "../../config/personal_info.json";
 
 export default function ProjectPage({
 	projectsList,
@@ -12,6 +14,7 @@ export default function ProjectPage({
 	return (
 		<DefaultLayout>
 			<Container maxWidth="max-w-6xl">
+				<SEO title={`Projects by ${personalInfo.name}`} />
 				<PageTitle title="Projects" />
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mb-14">
 					{projectsList.map((project, key) => (
