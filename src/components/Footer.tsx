c8e06f4f962751be825489747fc74f3292a5b3f0
Nihal Mittal <nihalmittal47@gmail.com>
2023-03-12 16:25:09 +0530

    feat: Added portfolio github repo link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { BsSuitHeartFill } from "react-icons/bs";
 import { PageSocial } from "./PageSocial";
+import Link from "next/link";
 import personalInfo from "../config/personal_info.json";
 
 export const Footer = () => {
@@ -7,11 +8,17 @@ export const Footer = () => {
 		<footer className="footer footer-center bg-base-200 text-base-content rounded px-4 py-6">
 			<PageSocial />
 			<div>
-				<p>
-					Made with &nbsp;
-					<BsSuitHeartFill className="inline" />
-					&nbsp; by {personalInfo.name}
-				</p>
+				<Link
+					href="https://github.com/codescientist703/portfolio-blog"
+					target="_blank"
+					aria-label="Github Repository Link"
+				>
+					<p>
+						Made with &nbsp;
+						<BsSuitHeartFill className="inline underline fill-red-500" />
+						&nbsp; by {personalInfo.name}
+					</p>
+				</Link>
 			</div>
 		</footer>
 	);
