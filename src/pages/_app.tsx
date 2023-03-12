9c5ecc1ed22d685fad315ae0e528fc3ee3fab10b
Nihal Mittal <nihalmittal47@gmail.com>
2023-03-12 17:14:57 +0530

    feat: add icons using https://favicon.io/

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,11 @@ export default function App({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+				<link rel="icon" href="/icons/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+				<link rel="manifest" href="/icons/site.webmanifest" />
 			</Head>
 			<Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
 			<Component {...pageProps} />
